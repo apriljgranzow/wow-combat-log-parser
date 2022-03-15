40577178cb1ff4374e1cd0a64ff0168af384b172
April Granzow <apriljgranzow@gmail.com>
2022-03-15 15:37:22 -0700

    Get all unique event type names

--- eventTypes.py
+++ eventTypes.py
@@ -3,10 +3,14 @@ import re
 def getEventType(line):
   return line.split('  ')[1].split(',')[0]
 
+def getAllUniqueEventTypes(lines):
+  events = set(map(getEventType, lines))
+  return events
+
 def splitEvent(line):
   pattern = re.compile()
 
 with open("/Users/aprilgranzow/wow-combat-log-parser/logFiles/WoWCombatLog-031322_160329.txt") as file:
   combatLog = file.read()
   lines = combatLog.splitlines()
-  print(map(getEventType, lines[0:10]))
+  print(sorted(list(getAllUniqueEventTypes(lines))))
