4b747f3e95d82790a3a75af87c069626d0c9f9f5
April Granzow <apriljgranzow@gmail.com>
2022-03-15 15:41:05 -0700

    Create functions to get list of args, peak top N lines of file

--- eventTypes.py
+++ eventTypes.py
@@ -7,10 +7,16 @@ def getAllUniqueEventTypes(lines):
   events = set(map(getEventType, lines))
   return events
 
+def getArgs(line):
+  return line.split('  ')[1].split(',')[1:]
+
 def splitEvent(line):
   pattern = re.compile()
+
+def peakLines(lines, numLines=10):
+  return lines[:numLines]
 
 with open("/Users/aprilgranzow/wow-combat-log-parser/logFiles/WoWCombatLog-031322_160329.txt") as file:
   combatLog = file.read()
   lines = combatLog.splitlines()
-  print(sorted(list(getAllUniqueEventTypes(lines))))
+  print(map(getArgs, peakLines(lines)))
